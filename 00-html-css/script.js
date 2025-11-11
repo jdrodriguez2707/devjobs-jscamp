698192e8e36ec32a1a0bb891ba9bc59d1b189629
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-11-11 09:13:42 -0500

    Añadir interactividad con JavaScript a la página de empleos
    Se añadió un nuevo script de JavaScript para agregarle interactividad a un botón de aplicar a trabajos.
    
    Se agregó type="module" al script para que se cargue en paralelo con el HTML optimizando así la carga.

---
+++ 00-html-css/script.js
@@ -0,0 +1,8 @@
+const message = "Hola";
+
+const button = document.querySelector("#button");
+button.addEventListener("click", () => {
+  alert(
+    "Gracias por su interés. Muy pronto nos pondremos en contacto con usted"
+  );
+});
