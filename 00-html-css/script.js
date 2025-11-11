a592be862f101a36bdd9ccadfccc25151c5f3b1a
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-11-11 13:02:29 -0500

    Añadir funcionalidad de filtro de ubicación en la página de empleos
    - Se añadió un nuevo filtro de ubicación que permite a los usuarios filtrar los trabajos según su modalidad (remoto o presencial).
    
    - Se implementaron clases CSS para mostrar y ocultar trabajos según el filtro seleccionado.
    
    - Se agregó un atributo data-location para cada job card para guardar información de la ubicación y así filtrarlos de manera más fácil.
    
    - Se agregó una opción en el select para seleccionar todas las
      ubicaciones.

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,36 +1,31 @@
-// const applyButton = document.querySelector("#button");
-// console.log(applyButton);
-
-// if (applyButton) {
-//   applyButton.addEventListener("click", () => {
-//     applyButton.textContent = "¡Aplicado!";
-//     applyButton.classList.add("is-applied");
-//     applyButton.disabled = true;
-//   });
-// }
-
-// const applyButtons = document.querySelectorAll(
-//   ".search-results__job-card-apply-btn"
-// );
-
-// applyButtons.forEach((applyButton) => {
-//   let isApplied = false;
-//   applyButton.addEventListener("click", () => {
-//     isApplied = isApplied ? false : true;
-//     applyButton.textContent = isApplied ? "¡Aplicado!" : "Aplicar";
-//     applyButton.classList.toggle("is-applied");
-//   });
-// });
-
 const searchResultsJobs = document.querySelector(
   ".search-results__jobs-container"
 );
 
-searchResultsJobs.addEventListener("click", (event) => {
+searchResultsJobs?.addEventListener("click", (event) => {
   const element = event.target;
   if (element.classList.contains("search-results__job-card-apply-btn")) {
     element.textContent = "¡Aplicado!";
     element.classList.add("is-applied");
     element.disabled = true;
   }
+});
+
+const jobLocationFilter = document.querySelector("#filter-location");
+jobLocationFilter?.addEventListener("change", () => {
+  const filterValue = jobLocationFilter.value;
+  const jobList = Array.from(searchResultsJobs.children); // Convertir a Array para poder usar forEach
+
+  jobList.forEach((job) => {
+    if (filterValue === job.dataset.location) {
+      job.classList.remove("hidden");
+      job.classList.add("visible");
+    } else if (filterValue && filterValue !== job.dataset.location) {
+      job.classList.remove("visible");
+      job.classList.add("hidden");
+    } else {
+      job.classList.remove("hidden");
+      job.classList.add("visible");
+    }
+  });
 });
