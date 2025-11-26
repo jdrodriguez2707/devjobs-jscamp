7fc30fd69228a39d0ccd31e74d881c79e654b2c2
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-11-26 15:36:56 -0500

    Mejorar logica de filtros para que el usuario pueda combinar varios al mismo tiempo

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -11,23 +11,48 @@ searchResultsJobs?.addEventListener("click", (event) => {
 
 const searchFilters = document.querySelector(".search-job__filters-container");
 
-searchFilters.addEventListener("change", (event) => {
-  const filter = event.target;
-  filter.children[0].hidden = true; // Ocultar primer option por defecto cuando el select cambie
-  const filterValue = filter.value;
-  const jobList = Array.from(searchResultsJobs.children); // Convertir a Array para poder usar forEach
-  const filterId = filter.id; // Obtener id del select seleccionado para acceder al dataset en especifico
+// searchFilters.addEventListener("change", (event) => {
+//   const filter = event.target;
+//   filter.children[0].hidden = true; // Ocultar primer option por defecto cuando el select cambie
+//   const filterValue = filter.value;
+//   const jobList = Array.from(searchResultsJobs.children); // Convertir a Array para poder usar forEach
+//   const filterId = filter.id; // Obtener id del select seleccionado para acceder al dataset en especifico
 
-  jobList.forEach((job) => {
-    if (filterValue === job.dataset[filterId]) {
-      job.classList.remove("hidden");
-      job.classList.add("visible");
-    } else if (filterValue && filterValue !== job.dataset[filterId]) {
-      job.classList.remove("visible");
-      job.classList.add("hidden");
+//   jobList.forEach((job) => {
+//     if (filterValue === job.dataset[filterId]) {
+//       job.classList.remove("hidden");
+//       job.classList.add("visible");
+//     } else if (filterValue && filterValue !== job.dataset[filterId]) {
+//       job.classList.remove("visible");
+//       job.classList.add("hidden");
+//     } else {
+//       job.classList.remove("hidden");
+//       job.classList.add("visible");
+//     }
+//   });
+// });
+
+searchFilters.addEventListener("change", (event) => {
+  const currentSelect = event.target;
+  currentSelect.children[0].hidden = true; // Ocultar primer option por defecto cuando el select cambie
+  const selects = Array.from(document.querySelectorAll(".search-job__filter-select"));
+  const selectValues = selects.map((select) => select.value);
+  console.log(selectValues);
+
+  const jobList = Array.from(searchResultsJobs.children); // Convertir a Array para poder usar forEach
+
+  for (let i = 0; i < jobList.length; i++) {
+    if (
+      (jobList[i].dataset.technology === selectValues[0] || selectValues[0] === "") &&
+      (jobList[i].dataset.location === selectValues[1] || selectValues[1] === "") &&
+      (jobList[i].dataset.contract === selectValues[2] || selectValues[2] === "") &&
+      (jobList[i].dataset.experience === selectValues[3] || selectValues[3] === "")
+    ) {
+      jobList[i].classList.remove("hidden");
+      jobList[i].classList.add("visible");
     } else {
-      job.classList.remove("hidden");
-      job.classList.add("visible");
+      jobList[i].classList.remove("visible");
+      jobList[i].classList.add("hidden");
     }
-  });
+  }
 });
