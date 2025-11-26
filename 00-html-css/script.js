b5bee5cc399ab306773f47edf3e6f32144dfcd90
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-11-26 14:36:16 -0500

    Añadir funcionalidad de filtro para el resto de selectores
    - Se añadió la funcionalidad de filtro para los selectores restantes (tecnologías, tipo de contrato y nivel de experiencia)
    
    - También se modificaron las opciones de los selectores para que cuando el usuario despliegue cada lista, aparezca como primera opción una para seleccionar todos los empleos sin filtro
    
    - Se eliminaron algunas opciones en el filtro de tecnología para hacerlo más sencillo

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,6 +1,4 @@
-const searchResultsJobs = document.querySelector(
-  ".search-results__jobs-container"
-);
+const searchResultsJobs = document.querySelector(".search-results__jobs-container");
 
 searchResultsJobs?.addEventListener("click", (event) => {
   const element = event.target;
@@ -11,16 +9,20 @@ searchResultsJobs?.addEventListener("click", (event) => {
   }
 });
 
-const jobLocationFilter = document.querySelector("#filter-location");
-jobLocationFilter?.addEventListener("change", () => {
-  const filterValue = jobLocationFilter.value;
+const searchFilters = document.querySelector(".search-job__filters-container");
+
+searchFilters.addEventListener("change", (event) => {
+  const filter = event.target;
+  filter.children[0].hidden = true; // Ocultar primer option por defecto cuando el select cambie
+  const filterValue = filter.value;
   const jobList = Array.from(searchResultsJobs.children); // Convertir a Array para poder usar forEach
+  const filterId = filter.id; // Obtener id del select seleccionado para acceder al dataset en especifico
 
   jobList.forEach((job) => {
-    if (filterValue === job.dataset.location) {
+    if (filterValue === job.dataset[filterId]) {
       job.classList.remove("hidden");
       job.classList.add("visible");
-    } else if (filterValue && filterValue !== job.dataset.location) {
+    } else if (filterValue && filterValue !== job.dataset[filterId]) {
       job.classList.remove("visible");
       job.classList.add("hidden");
     } else {
