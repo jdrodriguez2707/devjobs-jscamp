7d7616c53996136cba2be57431a52bbbec877d53
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-12-16 10:57:54 -0500

    Implementar carga dinámica de empleos desde archivo JSON y actualizar la lógica de visualización de tarjetas de trabajo

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const searchResultsJobs = document.querySelector(".search-results__jobs-container");
+const searchResultsJobContainer = document.querySelector(".search-results__jobs-container");
 const searchFiltersContainer = document.querySelector(".search-job__filters-container");
 const filterSelects = document.querySelectorAll(".search-job__filter-select");
 
-searchResultsJobs?.addEventListener("click", (event) => {
+// Obtener lista de empleos desde el archivo JSON
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jobs) => {
+    console.log(jobs);
+    jobs.forEach((job) => {
+      const articleJob = document.createElement("article");
+      articleJob.className = "search-results__job-card";
+      articleJob.dataset.location = job.data.location;
+      articleJob.dataset.technology = job.data.technology;
+      articleJob.dataset.contract = job.data.contract;
+      articleJob.dataset.experience = job.data.experience;
+
+      articleJob.innerHTML = `<div>
+            <h3 class="search-results__job-card-title">${job.titulo}</h3>
+            <p class="search-results__job-card-company">
+              <span>${job.empresa}</span> |
+              <span>${job.ubicacion}</span>
+            </p>
+            <p class="search-results__job-card-description">
+              ${job.descripcion}
+            </p>
+          </div>
+          <button class="search-results__job-card-apply-btn">Aplicar</button>`;
+
+      searchResultsJobContainer.appendChild(articleJob);
+    });
+  });
+
+searchResultsJobContainer?.addEventListener("click", (event) => {
   const element = event.target;
   if (element.classList.contains("search-results__job-card-apply-btn")) {
     element.textContent = "¡Aplicado!";
@@ -40,7 +71,7 @@ function filterJobOffers() {
     filterValues[filterSelect.id] = filterSelect.value;
   });
 
-  const jobList = Array.from(searchResultsJobs.children);
+  const jobList = Array.from(searchResultsJobContainer.children);
 
   const filterKeys = Object.keys(filterValues);
 
