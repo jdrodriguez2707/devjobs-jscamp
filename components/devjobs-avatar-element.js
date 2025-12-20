6b6b7ae38f56d0333c7b425fb52405d45246276b
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-12-20 14:33:24 -0500

    Agregar atributos al componente del avatar de usuario para hacerlo dinámico y más reutilizable
    - Se agregaron atributos de servicio, nombre de usuario y tamaño para poder iterar y crear más variantes del componente DevJobsAvatar con diferentes valores aprovechando el servicio de 'unavatar.io'
    
    - Se agregaron valores de respaldo por si no se le agregan atributos al componente para evitar valores nulos

--- components/devjobs-avatar-element.js
+++ components/devjobs-avatar-element.js
@@ -1,31 +1,37 @@
-// Siempre se debe heredar de HTMLElement para tener disponible todo lo que tiene una etiqueta 
-// HTML (métodos, propiedades, eventos etc.)
 class DevJobsAvatar extends HTMLElement {
   constructor() {
-    // Llamamos al constructor de HTMLElement para tener todo disponible (siempre debe ir primero)
-    super(); 
-    // Atamamos el componente al ShadowDOM para encapsularlo y aislarlo del DOM principal
-    // mode: "open" -> controla la accesibilidad del shadow DOM
-    // "open" quiere decir que será accesible por JS externo mientras que "closed" lo contrario
+    super();
     this.attachShadow({ mode: "open" });
   }
 
+  createUrl(service, username) {
+    return `https://unavatar.io/${service}/${username}`;
+  }
+
   render() {
-    // Insertamos el HTML en el shadowRoot que devuelve el shadowDOM (burbuja)
+    const service = this.getAttribute("service") ?? "github";
+    const username = this.getAttribute("username") ?? "jdrodriguez2707";
+    const size = this.getAttribute("size") ?? "4.8";
+    const url = this.createUrl(service, username);
+
     this.shadowRoot.innerHTML = `
+      <style>
+        img {
+          width: ${size}rem;
+          height: ${size}rem;
+          border-radius: 50%;
+        }
+      </style>
       <img 
-        src="https://unavatar.io/github/jdrodriguez2707"
-        alt="foto de perfil del usuario"
-        style="width: 40px; height: 40px; border-radius: 50%;"
+        src="${url}"
+        alt="Avatar de ${username}"
       >
     `;
   }
 
-  // Se ejecuta cuando se añade el componente al DOM
   connectedCallback() {
     this.render();
   }
 }
 
-// Registramos el componente para poder usarlo como etiqueta HTML en el navegador
 customElements.define("devjobs-avatar", DevJobsAvatar);
